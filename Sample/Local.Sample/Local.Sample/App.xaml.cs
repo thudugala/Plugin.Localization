c0d8e0d65807309be3ceec5d28e876786d83b8bd
Elvin Thudugala <elvin@outlook.co.nz>
2018-09-19 15:40:34 +1200

    sample solution

---
+++ Sample/Local.Sample/Local.Sample/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace Local.Sample
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
