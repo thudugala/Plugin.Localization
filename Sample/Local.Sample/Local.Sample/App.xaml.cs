171f484380771219763198c0755bf0bf175fdfcc
Elvin Thudugala <elvin@outlook.co.nz>
2018-09-25 14:49:05 +1200

    Added Plugin.Localization reference

--- Sample/Local.Sample/Local.Sample/App.xaml.cs
+++ Sample/Local.Sample/Local.Sample/App.xaml.cs
@@ -1,9 +1,14 @@
 ﻿using System;
 using System.Globalization;
+using Plugin.Localization;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+
+//translateManager only set Culture in resource's, if only this Attribute is set.
+[assembly: ResxLocalize]
+
 namespace Local.Sample
 {
     public partial class App : Application
@@ -11,7 +16,18 @@ namespace Local.Sample
         public App()
         {
             InitializeComponent();
-            
+
+            // Android and IOS projects automatically sets the resource's culture correctly to the CultureInfo.InstalledUICulture
+            // Also set the Culture in all the resource's in all the assemblies. 
+            // Only need to reference this package if that is not the case.
+
+            var localizeHelper = DependencyService.Resolve<ILocalizeHelper>();
+            var languageConvertor = new LanguageConvertor();
+            var translateManager = new TranslateManager(localizeHelper, languageConvertor);
+
+            // Set Culture in all the resource's in all the assemblies that has ResxLocalize Attribute.
+            translateManager.SetCulture();
+
             MainPage = new MainPage();
         }
 
